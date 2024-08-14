efdafc96d96621e293ddd0a51e1ace3e93c1f567
Giulio Vaccari <giuliov@amazon.com>
2024-08-14 15:29:56 +0200

    and aposss

--- src/components/OrderPage.js
+++ src/components/OrderPage.js
@@ -130,7 +130,7 @@ export default function OrderPage({ barId, tableNumber }) {
               onClick={startNewOrder}
               className="bg-blue-500 text-white px-6 py-2 rounded-full text-lg font-semibold hover:bg-blue-400 transition duration-300"
             >
-              {"Ordina qualcos'altro"}
+              Ordina qualcos&apos;altro
             </button>
           </motion.div>
         )}
