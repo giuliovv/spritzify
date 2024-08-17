40bbcd820d97ee1034589c1cd0068e80a126f13b
Pietro Fantini <pietro.fantini1998@gmail.com>
2024-08-17 02:48:52 +0200

    console log public key

--- src/components/OrderPage.js
+++ src/components/OrderPage.js
@@ -14,6 +14,8 @@ import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
+console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
 export default function OrderPage({ barId, tableNumber }) {
   const [bar, setBar] = useState(null);
   const [order, setOrder] = useState([]);
