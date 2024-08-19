71c9bacf1c211807f8aba2643d490e53390f7825
Giulio Vaccari <giuliov@amazon.com>
2024-08-19 17:04:51 +0200

    Payment with suspense

--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -1,6 +1,10 @@
 // src/app/payment/page.js
+
+import { Suspense } from 'react';
 import PaymentPage from '../../components/PaymentPage';
 
 export default function Payment() {
-  return <PaymentPage />;
+  return <Suspense fallback={<div>Loading...</div>}>
+      <PaymentPage />
+    </Suspense>;
 }
