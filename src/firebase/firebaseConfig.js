bc93dbc0976b9740bba906913459173361e268b4
Giulio Vaccari <giuliov@amazon.com>
2024-08-24 22:43:07 +0100

    Firebase with env

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,14 +5,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClOPIlYEhluvw1Dov4TU8DZbe5D6RQ1y4",
-  authDomain: "spritzify-6f52c.firebaseapp.com",
-  projectId: "spritzify-6f52c",
-  storageBucket: "spritzify-6f52c.appspot.com",
-  messagingSenderId: "511607028837",
-  appId: "1:511607028837:web:5596164fdf67daee061a8c"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
 
 let firebaseApp;
 if (!getApps().length) {
