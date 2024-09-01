b1ebdecd778e621b028d52f05154a5974b7f9d05
Giulio Vaccari <44315076+giuliovv@users.noreply.github.com>
2024-09-01 21:16:57 +0100

    Validate inputs (#24)
    Validate inputs and redirect in homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
+// /src/app/page.js
+
+"use client"
+
+import { useEffect } from 'react';
 import Footer from '@/components/Footer';
 import SelectTable from '../components/SelectTable';
 
 export default function Home() {
+  useEffect(() => {
+    window.location.href = 'https://spiaggiato.it';
+  }, []);
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-400 to-purple-500 flex flex-col justify-between p-4">
       <main className="bg-white bg-opacity-20 p-8 rounded-lg backdrop-blur-md w-full max-w-md mx-auto">
